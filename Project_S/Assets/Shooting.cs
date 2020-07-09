7c41cae1cdc37b59bed66d19377d691c385ada1b
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-09 19:48:41 +0200

    triple bullet

--- Project_S/Assets/Shooting.cs
+++ Project_S/Assets/Shooting.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
+    public Transform firePoint_2;
+    public Transform firePoint_3;
+
     public GameObject bulletPrefab;
 
     public float bulletForce = 20f;
@@ -19,8 +22,18 @@ public class Shooting : MonoBehaviour
 
     void Shoot() {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet_2 = Instantiate(bulletPrefab, firePoint_2.position, firePoint_2.rotation);
+        GameObject bullet_3 = Instantiate(bulletPrefab, firePoint_3.position, firePoint_3.rotation);
+
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+        Rigidbody2D rb_2 = bullet_2.GetComponent<Rigidbody2D>();
+        rb_2.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+        Rigidbody2D rb_3 = bullet_3.GetComponent<Rigidbody2D>();
+        rb_3.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
     }
 
 
