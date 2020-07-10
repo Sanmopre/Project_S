15aba25bdd4a784252a53a3530b220f85b1c7cc9
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-10 20:17:59 +0200

    fixed camera rotation

---
+++ Project_S/Assets/Camera_fixes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_fixes : MonoBehaviour
+{
+    public Transform camera_pos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        camera_pos.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
