a6f5ad95f0a8c7106409915a405d097ad65b8f60
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-16 21:20:34 +0200

    Sprint with stamina bar

--- Project_S/Assets/PlayerMovement.cs
+++ Project_S/Assets/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Camera cam;
 
+    public float sprint = 1.5f;
+    public int stamina = 500;
+    private bool sprinting = false;
+
     public float dashSpeed;
     private float dashTime;
     public float startDashTime;
@@ -17,18 +21,54 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
     Vector2 mousePos;
 
+    public StaminaBar stamina_bar;
+
+    private void Start()
+    {
+        stamina_bar.Set_Max_Stamina(stamina);
+    }
+
     // Update is called once per frame
     void Update()
     {
       movement.x = Input.GetAxisRaw("Horizontal");
       movement.y = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetButtonDown("Sprint"))
+        {
+            sprinting = true;
+        }
+
+        if (Input.GetButtonUp("Sprint"))
+        {
+            sprinting = false;
+        }
+
+        if (sprinting == true)
+        {
+            stamina = stamina - 2;
+        }
+        else {
+            stamina++;
+        }
+
+
+        stamina_bar.Set_Stamina(stamina);
+
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (sprinting == false)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime * sprint);
+        }
+
 
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
