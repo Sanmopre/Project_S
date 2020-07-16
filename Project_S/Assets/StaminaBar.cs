50e004285895383ee1b9a06a27685a76bdf807e1
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-16 21:05:24 +0200

    stamina bar

---
+++ Project_S/Assets/StaminaBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StaminaBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void Set_Stamina(int stamina) 
+    {
+        slider.value = stamina;
+    }
+
+    public void Set_Max_Stamina(int stamina)
+    {
+        slider.maxValue = stamina;
+        slider.value = stamina;
+    }
+
+}
