6a6506583a16bfa616ce9eb3086fd97a4ce0db04
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-22 13:02:11 +0200

    trail manager

--- Project_S/Assets/Abilities_manager.cs
+++ Project_S/Assets/Abilities_manager.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Abilities_manager : MonoBehaviour
 {
-
+    public TrailRenderer trail;
     public Sprite Ultimate_Sprite;
     public Sprite Normal_Sprite;
     public bool in_ultimate = false;
     public PlayerMovement mov;
+    private int trail_delay = 10;
+    private int counter = 10;
 
     void Update()
     {
 
         if (mov.sprinting)
         {
+            trail.enabled = true;
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Ultimate_Sprite;
+            counter = 15;
         }
         else {
+            if (counter < 0)
+            {
+                trail.enabled = false;
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Normal_Sprite;
         }
-
+        counter--;
     }
 }
