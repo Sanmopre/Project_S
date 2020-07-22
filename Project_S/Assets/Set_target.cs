9f463b1023d482b3a4f66699f693229e00f0624c
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-22 17:18:20 +0200

    enemy pathfinding working

---
+++ Project_S/Assets/Set_target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Set_target : MonoBehaviour
+{
+    private GameObject player;
+
+    public AIDestinationSetter character;
+    void Start()
+    {
+        player = GameObject.Find("Character");
+        character.target = player.transform;
+    }
+
+}
