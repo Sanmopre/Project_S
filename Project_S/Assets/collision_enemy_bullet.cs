6ac38ecf474e3f3203af8c548c921f4c7821f308
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-27 18:50:58 +0200

    enemies and bullets coll fix

--- Project_S/Assets/collision_enemy_bullet.cs
+++ Project_S/Assets/collision_enemy_bullet.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class collision_enemy_bullet : MonoBehaviour
 {
 
+
+    private void Update()
+    {
+        Physics2D.IgnoreLayerCollision(11, 12);
+    }
+
     public int damage;
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Player_Health player = collider.GetComponent<Player_Health>();
         PlayerMovement move = collider.GetComponent<PlayerMovement>();
         Health enemy = collider.GetComponent<Health>();
+
+
 
         if (player != null)
         {
