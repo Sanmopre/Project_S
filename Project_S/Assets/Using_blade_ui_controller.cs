3602dbd34f04be21c2e3f4b358d96e03636bce12
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-27 16:03:03 +0200

    ui improv abilities

---
+++ Project_S/Assets/Using_blade_ui_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Using_blade_ui_controller : MonoBehaviour
+{
+    public Image image;
+    public PlayerMovement mov;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mov.sprinting)
+        {
+            image.enabled = true;
+        }
+        else {
+            image.enabled = false;
+        }
+    }
+}
