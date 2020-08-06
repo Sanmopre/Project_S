cdebac0d1bf6e4239b11c212b50aff7c56ecbbb5
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-06 16:08:19 +0200

    finished Gameplay

--- Project_S/Assets/Object_Spawner.cs
+++ Project_S/Assets/Object_Spawner.cs
@@ -15,6 +15,9 @@ public class Object_Spawner : MonoBehaviour
     public Vector2 pos_3;
     public Vector2 pos_4;
 
+    private Vector2 position;
+    int count_health = 1;
+    int count_energy = 1;
 
     public float spawn_rate_h = 2f;
     public float spawn_rate_e = 2f;
@@ -29,22 +32,35 @@ public class Object_Spawner : MonoBehaviour
         {
 
             nextSpawn_h = Time.time + spawn_rate_h;
-            randX = Random.Range(-20, 20);
-            randY = Random.Range(2, -35);
 
-            pos_1 = new Vector2(randX, randY);
-            Instantiate(health, pos_1, Quaternion.identity);
+            if (count_health % 2 == 0)
+            {
+                position = pos_1;
+            }
+            else {
+                position = pos_3;
+            }
+            Instantiate(health, position, Quaternion.identity);
+            count_health++;
         }
 
         if (Time.time > nextSpawn_e)
         {
 
             nextSpawn_e = Time.time + spawn_rate_e;
-            randX = Random.Range(-20, 20);
-            randY = Random.Range(2, -35);
 
-            pos_1 = new Vector2(randX, randY);
-            Instantiate(energy, pos_1, Quaternion.identity);
+
+            if (count_energy % 2 == 0)
+            {
+                position = pos_2;
+            }
+            else
+            {
+                position = pos_4;
+            }
+
+            Instantiate(energy, position, Quaternion.identity);
+            count_energy++;
         }
 
     }
