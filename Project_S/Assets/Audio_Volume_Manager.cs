8b3c0ad5286eb2213af8631ff4d9c2d074930679
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-17 14:56:26 +0200

    Volume scroll

---
+++ Project_S/Assets/Audio_Volume_Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Audio_Volume_Manager : MonoBehaviour
+{
+    public Slider volume;
+
+    public AudioSource Music;
+
+    void Update()
+    {
+        Music.volume = volume.value;
+    }
+}
